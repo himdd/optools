5b8e1c83e31a388327f6d285c734989fe36a5375
himdd <himdd@himdddeMac.local>
2015-12-03 22:20:18 +0800

    start

--- script/install.sh
+++ script/install.sh
@@ -2,8 +2,8 @@
 #miaodongdong
 bashrc=$HOME"/.bashrc"
 file_path=$(dirname $(readlink -f $0))
-mg_path=$file_path"/mgtools.sh"
+op_path=$file_path"/optools.sh"
 if [ ! -f "$bashrc" ]; then
 	touch $bashrc
 fi
-echo "alias mgtools=$mg_path" >> $bashrc
+echo "alias optools=$op_path" >> $bashrc
